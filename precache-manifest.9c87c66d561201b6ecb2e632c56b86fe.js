920170872bbb4aa1b5b49e299d874addccb957f6
unknown <engrayanhnide98@gmail.com>
2023-06-03 23:11:14 +0300

    Updates

---
+++ precache-manifest.9c87c66d561201b6ecb2e632c56b86fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab66f99c5c6e07fc09aa",
+    "url": "/portfolio/static/css/main.698123e4.chunk.css"
+  },
+  {
+    "revision": "ab66f99c5c6e07fc09aa",
+    "url": "/portfolio/static/js/main.ab66f99c.chunk.js"
+  },
+  {
+    "revision": "d3553346d31b45835686",
+    "url": "/portfolio/static/js/1.d3553346.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a02554ff2d2300502816287220863c99",
+    "url": "/portfolio/index.html"
+  }
+];
