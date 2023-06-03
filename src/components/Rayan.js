e822f971cd628e02553d5396efb8b8c31594ed34
unknown <engrayanhnide98@gmail.com>
2023-06-03 22:53:18 +0300

    first commit

---
+++ src/components/Rayan.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Particles from "react-particles"
+import particles1 from "./ReactParticles";
+export default()=>{
+  return(
+    <>
+    
+    <Particles params={particles1}/>
+    </>
+  )
+}
