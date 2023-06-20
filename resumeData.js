b9f4a43eb6cac87c192248cd15fd22fb19a157f3
rayanHnide-FE1 <eng.rayanhnide98@gmail.com>
2023-06-20 18:03:12 +0300

    Add files via upload

---
+++ resumeData.js
@@ -0,0 +1,146 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Rayan Hnide",
+    "role": "Frontend Developer and UI/UX Designer",
+    "linkedinId":"https://www.linkedin.com/in/rayan-hnide-734586241",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and design and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rayan-hnide-734586241",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/rayanHnide-FE1",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/rayan.hnide",
+          "className":"fa fa-facebook"
+        },
+      
+
+      ],
+    "aboutme":"I am currently a final year student at the University of Damascus, Computer Engineering and Automation, specializing in automatic control and industrial automation. I am a front-end developer, and I am currently deepening the field to gain the largest amount of experience. I work with the xcore Corporation as a group leader in the Informatics Department Web-Front-End, and I supervised the training of more than Training workshop in the institution. I believe that to be successful in life one needs to be obsessed with their dreams and keep working towards them.",
+    "address":"Syria",
+    "website":"https://github.com/rayanHnide-FE1",
+    "education":[
+      {
+        "UniversityName":"Computer and automation engineering  ",
+        "specialization":"specialization in automatic control and industrial automation",
+        
+        "Achievements":"I completed plc course at mr-controller company and got a score of 92. I completed a workshop in robotics and I have completed a course in C++ language"
+      },
+   
+    ],
+    "work":[
+      {
+        "CompanyName":"XCORE Company",
+        "specialization":"Team Leader at Web Front End",
+        // "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"I supervised the training of more than 4 training workshops in developing websites.  I completed more than one project inside and outside Syria with the Foundation's team, and I also participated in the realization of a platform for the Syrian International Virtual School with the team"
+      },
+      {
+        "CompanyName":"ICR Company",
+        "specialization":"Front-End Web Development Trainer",
+        // "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2022",
+        "Achievements":"I supervised the training of a training workshop in web development. I supervised the training of a training workshop on the React JS framework"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5 90%"
+      },
+      {
+        "skillname":"CSS 80%"
+      },
+      {
+        "skillname":"Bootstrap 90%"
+      },
+      {
+        "skillname":"JsavaScript 50%"
+      },
+      {
+        "skillname":"Jquery 40%"
+      },
+      {
+        "skillname":"Reactjs 70%"
+      },
+      {
+        "skillname":"Redux 40%"
+      },
+      {
+        "skillname":"Redux toolkit 40%"
+      },
+      {
+        "skillname":"Mui 80%"
+      },
+      {
+        "skillname":"React Rsuite 80%"
+      },
+      {
+        "skillname":"Chakra UI 40%"
+      },
+      {
+        "skillname":"TypeScript 20%"
+      },
+      {
+        "skillname":"All React hooks like useState useLocation useParams useEffect useMemo elc..."
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"React App",
+        "imgurl":"images/portfollio/impact.png",
+        "bla": "https://thirdproject-c18f7.web.app/"
+      },
+      {
+        "name":"project2",
+        "description":"React App",
+        "imgurl":"images/portfollio/day.png",
+        "bla": "https://secondproject-4d56f.web.app/"
+
+      },
+      {
+        "name":"project3",
+        "description":"Without FrmeWork",  
+        "imgurl":"images/portfollio/startbootstrap.png",
+        "bla": "https://rayanhnide-fe1.github.io/Fitrst-Project/"
+
+      },
+      {
+        "name":"project4",
+        "description":"React App",
+        "imgurl":"images/portfollio/arsha.png",
+        "bla":"https://hotel-45299.web.app/"
+        
+      },
+      {
+        "name":"project5",
+        "description":"Crud App",
+        "imgurl":"images/portfollio/Crud.png",
+        "bla":"https://crud-app-ee322.web.app/"
+        
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Project completion on time"
+      },
+    
+    ]
+  }
+  
+  export default resumeData
